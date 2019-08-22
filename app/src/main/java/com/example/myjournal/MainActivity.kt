cd767e2a82f838bf8b4fbe7d87681df3266f0155
John Kelley <john.r.kelley@lowes.com>
2019-08-22 14:26:05 -0400

    Add registering of a user

--- app/src/main/java/com/example/myjournal/MainActivity.kt
+++ app/src/main/java/com/example/myjournal/MainActivity.kt
@@ -1,17 +1,20 @@
 package com.example.myjournal
 
 import android.os.Bundle
+import android.os.PatternMatcher
+import android.util.Log
+import android.util.Patterns
 //import android.support.v7.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
 import android.view.View
 import android.widget.Button
 import android.widget.EditText
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
 
-import kotlinx.android.synthetic.main.activity_main.*
 import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
 
@@ -33,7 +36,58 @@ class MainActivity : AppCompatActivity() {
 
     //ToDo relocate this if possible
     private lateinit var auth: FirebaseAuth
+    private lateinit var regButton: Button
+    private lateinit var regUserText: EditText
+    private lateinit var regPasswordText: EditText
+    private lateinit var regConfirmPasswordText: EditText
+    private fun signUpUser(){
+        Toast.makeText(baseContext, "Test3.",
+                        Toast.LENGTH_SHORT).show()
 
+        if(regUserText.text.toString().isEmpty()){
+            regUserText.error = "Please enter an email"
+            regUserText.requestFocus()
+            return
+        }
+        if(!Patterns.EMAIL_ADDRESS.matcher(regUserText.text.toString()).matches()){
+            regUserText.error = "Please enter a valid email"
+            regUserText.requestFocus()
+            return
+        }
+        if(regPasswordText.text.toString().isEmpty()){
+            regPasswordText.error = "Please enter a password"
+            regPasswordText.requestFocus()
+            return
+        }
+        if(regConfirmPasswordText.text.toString().isEmpty()){
+            regConfirmPasswordText.error = "Please confirm your password"
+            regConfirmPasswordText.requestFocus()
+            return
+        }
+        if(!regConfirmPasswordText.text.toString().equals(regPasswordText.text.toString())){
+            regConfirmPasswordText.error = "Your passwords do not match"
+            regConfirmPasswordText.requestFocus()
+            return
+        }
+        auth.createUserWithEmailAndPassword(regUserText.text.toString(), regPasswordText.text.toString())
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    // Sign in success, update UI with the signed-in user's information
+                   // Log.d(TAG, "createUserWithEmail:success")
+                    Toast.makeText(baseContext, "Authentication was successful.",
+                        Toast.LENGTH_SHORT).show()
+                    loginHandler.login(loginView,regView)
+                } else {
+                    // If sign in fails, display a message to the user.
+                   // Log.w(TAG, "createUserWithEmail:failure", task.exception)
+                    Toast.makeText(baseContext, "Authentication failed.",
+                        Toast.LENGTH_SHORT).show()
+                }
+
+                // ...
+            }
+    }
+    //--------------------
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -42,6 +96,15 @@ class MainActivity : AppCompatActivity() {
 
         //ToDo relocate this if possible
         auth = FirebaseAuth.getInstance()
+        regButton = findViewById(R.id.regButton)
+        regPasswordText = findViewById(R.id.regPasswordText)
+        regUserText = findViewById(R.id.regUserText)
+        regConfirmPasswordText = findViewById(R.id.regConfirmPasswordText)
+        regButton.setOnClickListener{
+            signUpUser()
+        }
+
+        //----------------
 
         dateTextName = findViewById(R.id.dateText)
         tagsTextName = findViewById(R.id.tagsText)
@@ -74,7 +137,6 @@ class MainActivity : AppCompatActivity() {
         super.onStart()
         // Check if user is signed in (non-null) and update UI accordingly.
         val currentUser = auth.currentUser
-        updateUI(currentUser)
     }
 
 
@@ -92,10 +154,5 @@ class MainActivity : AppCompatActivity() {
             R.id.action_settings -> true
             else -> super.onOptionsItemSelected(item)
         }
-    }
-
-    //ToDo relocate this if possible
-    fun updateUI(currentUser: FirebaseUser?){
-
     }
 }
