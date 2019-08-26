40ddbf8fc3e8e120dca2c3cc48a511a40aaceed9
John Kelley <john.r.kelley@lowes.com>
2019-08-25 23:46:15 -0400

    Added default timestamp

--- app/src/main/java/com/example/myjournal/MainActivity.kt
+++ app/src/main/java/com/example/myjournal/MainActivity.kt
@@ -1,46 +1,33 @@
 package com.example.myjournal
 
 import android.os.Bundle
-import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
 import android.widget.Button
 import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.auth.FirebaseUser
-
-import java.time.LocalDateTime
-import java.time.format.DateTimeFormatter
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var dateTextName: EditText
     lateinit var tagsTextName: EditText
     lateinit var entryTextName: EditText
     lateinit var saveButton: Button
     lateinit var currentUserId: String
     val TAG: String = "Main"
     private lateinit var auth: FirebaseAuth
-    val simpleFormat = DateTimeFormatter.ofPattern("M/d/yy")
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         auth = FirebaseAuth.getInstance()
-
-        dateTextName = findViewById(R.id.dateText)
         tagsTextName = findViewById(R.id.tagsText)
         entryTextName = findViewById(R.id.entryText)
         saveButton = findViewById(R.id.saveButton)
 
-        val today = LocalDateTime.now()
-        val formatted = today.format(simpleFormat)
-        dateTextName.setText(formatted.toString())
-
         saveButton.setOnClickListener {
             val entry = Entry()
-            entry.saveEntry(dateTextName, tagsTextName, entryTextName, this@MainActivity,currentUserId)
+            entry.saveEntry(tagsTextName, entryTextName, this@MainActivity,currentUserId)
         }
     }
 
