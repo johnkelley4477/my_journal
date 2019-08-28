e8b6fd14cd6859fee2ca9a497e3bd56643ce4cde
John Kelley <john.r.kelley@lowes.com>
2019-08-28 10:57:41 -0400

    Finish update

--- app/src/main/java/com/example/myjournal/DisplayJournalEntry.kt
+++ app/src/main/java/com/example/myjournal/DisplayJournalEntry.kt
@@ -4,9 +4,13 @@ import android.app.AlertDialog
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
+import android.view.ViewGroup
 import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.FirebaseDatabase
 
 class DisplayJournalEntry: AppCompatActivity() {
     val TAG = "DisplayJournalEntry"
@@ -28,7 +32,6 @@ class DisplayJournalEntry: AppCompatActivity() {
             update.setOnClickListener{
                 showUpdateDialog(jEntry)
             }
-
             date.text = getFormatedDate(jEntry[1]) + " " + getFormatedTime(jEntry[1])
             tags.text = jEntry[2].substring(1, jEntry[2].length - 1).replace(",", "")
             entry.text = jEntry[3]
@@ -42,9 +45,7 @@ class DisplayJournalEntry: AppCompatActivity() {
         val view = inflater.inflate(R.layout.layout_update_entry,  null)
         val editTextTags = view.findViewById<EditText>(R.id.tagsDialogText)
         val editTextEntry = view.findViewById<EditText>(R.id.entryDialogText)
-        val editTextDate = view.findViewById<EditText>(R.id.dateDialogText)
 
-        editTextDate.setText(getFormatedDate(jEntry[1]) + " " + getFormatedTime(jEntry[1]))
         editTextTags.setText(jEntry[2].substring(1, jEntry[2].length - 1).replace(",", ""))
         editTextEntry.setText(jEntry[3])
 
@@ -54,7 +55,24 @@ class DisplayJournalEntry: AppCompatActivity() {
         }
 
         builder.setPositiveButton("Update") {p0,P1 ->
+            val auth = FirebaseAuth.getInstance()
+            val currentUser = auth.currentUser
+            if(currentUser != null) {
+                val fdb = FirebaseDatabase.getInstance().getReference("${currentUser.uid}/entry")
+                val tags = if(editTextTags.text.isEmpty()) null else editTextTags.text.split(" ")
+                val entry = editTextEntry.text.toString().trim()
 
+                if(entry.isEmpty()){
+                    editTextEntry.error = "Please enter an Entry"
+                    editTextEntry.requestFocus()
+                    return@setPositiveButton
+                }
+                val journalEntry = JournalEntry(jEntry[0],jEntry[1],entry,tags)
+
+                fdb.child(jEntry[0]).setValue(journalEntry)
+
+                Toast.makeText(this,"Our journal entry has been updated",Toast.LENGTH_LONG).show()
+            }
         }
 
         val alert = builder.create()
@@ -81,5 +99,4 @@ class DisplayJournalEntry: AppCompatActivity() {
             return ""
         }
     }
-
 }
